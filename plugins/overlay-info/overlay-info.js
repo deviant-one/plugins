13cc89df9b8a7a8dc282c1bface8582cd89661bf
feederbox826 <feederbox826@users.noreply.github.com>
2026-04-19 00:23:31 -0400

    [overlay-info] left hand assignment

--- plugins/overlay-info/overlay-info.js
+++ plugins/overlay-info/overlay-info.js
@@ -21,7 +21,7 @@ function overlay_info() {
     const overlayResolution = SceneSpecs?.props?.children?.[1]
     if (!overlayResolution) return SceneSpecs // fallthrough in case it's undefined?
     const resolution = getResolutionClassName(file.height)
-    overlayResolution?.props?.className += ` ${resolution}`
+    overlayResolution.props.className += ` ${resolution}`
     // pull codec info
     const codec = file?.video_codec?.toUpperCase()
     // add codec to overlay
